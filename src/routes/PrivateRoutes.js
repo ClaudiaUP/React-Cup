a9042a69df0fde02f666909c0939d68408d50f56
claudiaUp <claudiaulled@hotmail.com>
2023-03-31 08:59:38 -0300

    navegacion 9

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -5,6 +5,7 @@ import Contacto from "../Components/Contacto/Contacto";
 import ItemDetailContainer from "../Components/ItemDetailContainer/ItemDetailContainer";
 import Cart from "../Components/Cart/Cart";
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Checkout from "../Components/Checkout/Checkout";
 
 
 const PrivateRoutes = () => {
@@ -18,6 +19,7 @@ const PrivateRoutes = () => {
                 <Route path="/productos/:categoriaId" element={ <ItemListContainer /> }/>
                 <Route path="/detail/:itemId" element={ <ItemDetailContainer /> }/>
                 <Route path="/cart" element={ <Cart /> } />
+                <Route path="/checkout" element={ <Checkout /> } />
                 <Route path="/nosotros" element={ <Nosotros /> } />
                 <Route path="/contacto" element={ <Contacto /> } />
                 <Route path="*" element={ <Navigate to="/" /> }/>
