34a1660b004eaad4b421b5d0d229d9b027281000
claudiaUp <claudiaulled@hotmail.com>
2023-04-03 09:31:44 -0300

    navegacion 13

--- src/firebase/upload.js
+++ src/firebase/upload.js
@@ -1,2 +1,15 @@
 
-console.log ("Hola Mundo")
+import MOCK_DATA from '../data/MOCK_DATA.json' assert { type: 'json'}
+import { db } from './config.js'
+import { addDoc, collection } from 'firebase/firestore'
+
+const data = MOCK_DATA.map((item) => {
+    delete item.Id
+    return item
+})
+
+const productRef = collection (db, 'productos')
+
+data.forEach ((item) => {
+    addDoc(productRef,item)
+})
